9825f1aa10995d9ef75afe31df51c0c356ee203b
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-01 12:55:32 +0200

    Update posts.py
    Read one single post

--- backend/routers/posts.py
+++ backend/routers/posts.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlmodel import Session, select
 
 from database import Post, PostCreate, PostRead, engine
@@ -27,3 +27,11 @@ def read_posts():
         posts = session.exec(select(Post)).all()
         return posts
 
+
+@router.get("/{post_id}", response_model=PostRead)
+def read_post(post_id: int):
+    with Session(engine) as session:
+        post = session.get(Post, post_id)
+        if not post:
+            raise HTTPException(status_code=404, detail="Post not found")
+        return post
