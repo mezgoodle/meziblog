1ec6711a8f8abc9b5bc94629ddb82ce783fa4580
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-01 15:51:21 +0200

    Update index.js
    Get data from server

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,24 @@
 import Head from "next/head";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div>
       <Head>
         <title>Create Next App</title>
       </Head>
-      Hello
+      <div className="posts">
+        {posts.map((post, index) => (
+          <h3>{post.title}</h3>
+        ))}
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://127.0.0.1:8000/posts");
+  const posts = await response.json();
+  return {
+    props: { posts },
+  };
 }
