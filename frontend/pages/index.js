84d28a7f625834f31a31e6630ef64ace3daf6fc9
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-04 14:49:33 +0200

    Some updates

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const response = await fetch("http://127.0.0.1:8000/posts");
   const posts = await response.json();
   return {
--- frontend/pages/post/[id].js
+++ frontend/pages/post/[id].js
@@ -85,41 +85,8 @@ export default function Post({ post }) {
         </section>
       </div>
     </main>
-    // {/* <>
-    //   <Link href="/">
-    //     <a className="btn btn-back">Go back</a>
-    //   </Link>
-    //   <div className="card card-page">
-    //     <h1 className="post-title">{post.title}</h1>
-    //     <div className="post-date">
-    //       Posted on {new Date(post.created_at).toLocaleString()}
-    //     </div>
-    //     <div className="post-date">
-    //       Updated on {new Date(post.updated_at).toLocaleString()}
-    //     </div>
-    //     <div className="post-body">
-    //       <div>{post.body}</div>
-    //     </div>
-    //   </div>
-    // </> */}
   );
 }
-
-// export async function getStaticPaths() {
-//   const response = await fetch("http://127.0.0.1:8000/posts");
-//   const posts = await response.json();
-
-//   const paths = posts.map((post) => ({
-//     params: {
-//       id: post.id.toString(),
-//     },
-//   }));
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
 
 export async function getServerSideProps({ params: { id } }) {
   const response = await fetch(`http://127.0.0.1:8000/post/${id}`);
