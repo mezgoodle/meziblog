a35f6553677e878b7727cba472622c36ab22ae4c
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-06 11:38:45 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,27 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import create_db_and_tables
 from routers import posts
 
+
 app = FastAPI()
 
 app.include_router(posts.router)
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
