b87b6e993788a73156f737983f6835b50ad0140c
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-06 16:47:33 +0200

    Update users.py

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi import APIRouter, HTTPException, Depends, status, Query
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session, select
+from pydantic import EmailStr
 
-from database import UserCreate, UserRead, User, get_session, engine
+from database import UserCreate, UserRead, User, UserUpdate, get_session
 from auth_token import create_access_token
 from hashing import Hash
+
+from typing import List
 
 
 router = APIRouter(
@@ -33,3 +36,47 @@ async def login(*, session: Session = Depends(get_session), request: OAuth2Passw
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Incorrect password')
     access_token = create_access_token(data={"sub": user.email})
     return {'access_token': access_token, 'token_type': 'bearer'}
+
+
+@router.get("s", response_model=List[UserRead])
+async def read_users(*, session: Session = Depends(get_session), offset: int = 0, limit: int = Query(default=100, lte=100)):
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
+
+
+@router.get("/{user_email}", response_model=UserRead)
+async def read_user(*, session: Session = Depends(get_session), user_email: EmailStr):
+    statement = select(User).where(User.email == user_email)
+    results = session.exec(statement)
+    user = results.first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@router.patch("/{user_email}", response_model=UserRead)
+async def update_post(*, session: Session = Depends(get_session), user_email: EmailStr, user: UserUpdate):
+    statement = select(User).where(User.email == user_email)
+    results = session.exec(statement)
+    db_user = results.first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_data = user.dict(exclude_unset=True)
+    for key, value in user_data.items():
+        setattr(db_user, key, value)
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+
+@router.delete("/{user_email}")
+async def delete_post(*, session: Session = Depends(get_session), user_email: EmailStr):
+    statement = select(User).where(User.email == user_email)
+    results = session.exec(statement)
+    user = results.first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
+    session.commit()
+    return {"ok": True}
