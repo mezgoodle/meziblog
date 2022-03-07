91435f98bcd6963105187a70743994df8adc7fce
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-07 21:03:05 +0200

    Create context

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 import Layout from "../components/Layout";
+import { EditContext } from "../context/EditContext";
 
 function MyApp({ Component, pageProps }) {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <EditContext.Provider value={[title, setTitle]}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </EditContext.Provider>
   );
 }
 
