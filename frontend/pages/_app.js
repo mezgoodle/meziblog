dd377560a9a5658fbe5880ba6e7fd1bc232faed7
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-10 21:52:19 +0200

    Revert "Try the context"
    This reverts commit 502b818024326259dfa66f42b322668a92ddad09.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,16 +1,13 @@
 import Layout from "../components/Layout";
 import { EditProvider } from "../context/EditContext";
-import { UserProvider } from "../context/UserContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <EditProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </EditProvider>
-    </UserProvider>
+    <EditProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </EditProvider>
   );
 }
 
