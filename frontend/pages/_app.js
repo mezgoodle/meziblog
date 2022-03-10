502b818024326259dfa66f42b322668a92ddad09
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-10 18:51:41 +0200

    Try the context

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,16 @@
 import Layout from "../components/Layout";
 import { EditProvider } from "../context/EditContext";
+import { UserProvider } from "../context/UserContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <EditProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </EditProvider>
+    <UserProvider>
+      <EditProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </EditProvider>
+    </UserProvider>
   );
 }
 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,21 +1,57 @@
 import Head from "next/head";
 import Post from "../components/Post";
+import { useContext, useEffect, useState } from "react";
 import { sortByDate } from "../utils";
+import { UserContext } from "../context/UserContext";
+import Link from "next/link";
 
-export default function Home({ posts }) {
+export default function Home() {
+  const [posts, setPosts] = useState([{}]);
+  const { token } = useContext(UserContext);
+
+  useEffect(async () => {
+    if (token) {
+      const getPosts = async () => {
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        };
+        const response = await fetch(
+          "http://127.0.0.1:8000/posts",
+          requestOptions
+        );
+        const posts = await response.json();
+        return posts;
+      };
+      const posts = await getPosts();
+      setPosts(posts.sort(sortByDate));
+    }
+  }, []);
+
   return (
-    <div className="row justify-content-center">
-      {posts.map((post, index) => (
-        <Post post={post} key={index} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>Posts</title>
+      </Head>
+      {token ? (
+        <div className="row justify-content-center">
+          {posts.map((post, index) => (
+            <Post post={post} key={index} />
+          ))}
+        </div>
+      ) : (
+        <div className="text-center">
+          <p className="display-1">
+            Go to{" "}
+            <Link href={"/user"}>
+              <a>login page</a>
+            </Link>
+          </p>
+        </div>
+      )}
+    </>
   );
-}
-
-export async function getServerSideProps() {
-  const response = await fetch("http://127.0.0.1:8000/posts");
-  const posts = await response.json();
-  return {
-    props: { posts: posts.sort(sortByDate) },
-  };
 }
