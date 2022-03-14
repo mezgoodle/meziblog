03624cc017efdf0f156d820e6f08b298f827edcf
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-14 16:57:19 +0200

    Update [id].js

--- frontend/pages/post/[id].js
+++ frontend/pages/post/[id].js
@@ -1,5 +1,6 @@
 import { useState, useContext, useEffect } from "react";
 import Link from "next/link";
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 import PostBody from "../../components/PostBody";
@@ -49,62 +50,70 @@ export default function Post({ data }) {
   }, [data]);
 
   return (
-    <main className="mt-3 pt-3">
-      <div className="container">
-        <section className="mt-4">
-          <div className="row">
-            <div className="col-md mb-4">
-              <div className="card mb-4 wow fadeIn">
-                <img
-                  src="https://mdbootstrap.com/img/Photos/Slides/img%20(144).jpg"
-                  className="img-fluid"
-                  alt=""
-                />
-              </div>
-              <div className="card mb-4 wow fadeIn">
-                {!editable ? (
-                  <PostBody post={post} />
-                ) : (
-                  <EditPost post={post} />
-                )}
-                <div className="mb-3 ms-3">
-                  <Link href="/">
-                    <a>
-                      <button
-                        type="button"
-                        className="btn btn-dark btn-rounded"
-                      >
-                        Back to posts
-                      </button>
-                    </a>
-                  </Link>
-                  {token ? (
-                    <>
-                      <button
-                        type="button"
-                        className="btn btn-warning btn-rounded ms-3"
-                        onClick={handleEdit}
-                      >
-                        {!editable ? "Update the post" : "Submit the updating"}
-                      </button>
-                      <button
-                        type="button"
-                        className="btn btn-danger btn-rounded ms-3"
-                        onClick={handleDelete}
-                      >
-                        Delete the post
-                      </button>
-                    </>
+    <>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+
+      <main className="mt-3 pt-3">
+        <div className="container">
+          <section className="mt-4">
+            <div className="row">
+              <div className="col-md mb-4">
+                <div className="card mb-4 wow fadeIn">
+                  <img
+                    src="https://mdbootstrap.com/img/Photos/Slides/img%20(144).jpg"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
+                <div className="card mb-4 wow fadeIn">
+                  {!editable ? (
+                    <PostBody post={post} />
                   ) : (
-                    ""
+                    <EditPost post={post} />
                   )}
+                  <div className="mb-3 ms-3">
+                    <Link href="/">
+                      <a>
+                        <button
+                          type="button"
+                          className="btn btn-dark btn-rounded"
+                        >
+                          Back to posts
+                        </button>
+                      </a>
+                    </Link>
+                    {token ? (
+                      <>
+                        <button
+                          type="button"
+                          className="btn btn-warning btn-rounded ms-3"
+                          onClick={handleEdit}
+                        >
+                          {!editable
+                            ? "Update the post"
+                            : "Submit the updating"}
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-danger btn-rounded ms-3"
+                          onClick={handleDelete}
+                        >
+                          Delete the post
+                        </button>
+                      </>
+                    ) : (
+                      ""
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
-      </div>
-    </main>
+          </section>
+        </div>
+      </main>
+    </>
   );
 }
 
