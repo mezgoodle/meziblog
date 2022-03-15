e2b7d4f8a8c9ea095b4b053b971b28dc9789d43c
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-15 12:52:26 +0200

    Update test_posts.py

--- backend/tests/test_posts.py
+++ backend/tests/test_posts.py
@@ -6,8 +6,16 @@ client = TestClient(app)
 
 
 def test_get_post():
-    response = client.get('/post/1')
+    response = client.get('/post/2')
     assert response.status_code == 200
+    assert response.json() == {
+        "title": "new test",
+        "author_name": "mezgoodle",
+        "body": "string",
+        "id": 2,
+        "created_at": "2022-03-08T07:26:21.092971",
+        "updated_at": "2022-03-11T09:18:17.861890",
+    }
 
 
 def test_get_posts():
