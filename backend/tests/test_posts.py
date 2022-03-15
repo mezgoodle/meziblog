2630671971ae0b84867139d50228b906df3fa64f
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-15 12:48:17 +0200

    Update test_posts.py

--- backend/tests/test_posts.py
+++ backend/tests/test_posts.py
@@ -6,6 +6,10 @@ client = TestClient(app)
 
 
 def test_get_post():
-    response = client.get('/')
+    response = client.get('/post/1')
     assert response.status_code == 200
 
+
+def test_get_posts():
+    response = client.get('/posts')
+    assert response.status_code == 200
